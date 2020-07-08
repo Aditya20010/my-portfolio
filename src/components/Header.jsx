933dfbe4e698a24d52e1f91604f69eafa1e27981
Aditya20010 <bhatnagar.aditya20010@gmail.com>
2020-07-08 16:23:39 +0000

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+function header(){
+    return <div className="header-div">
+    
+        <h1>I'm Aditya.</h1>
+        <p>a web designer.</p>
+    </div>
+}
+export default header;
